a3f6973127b01103534149298178a4fa38a39842
ishahah06 <shah.ishagpi@gmail.com>
2026-08-26 02:22:58 +0530

    Add files via upload

---
+++ practical 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"**************"<<endl;
+   cout<<"  STUDENT RECORD MANAGEMENT SYSTEM"<<endl;
+   cout<<"**************"<<endl;
+
+
+   cout<<"Enter Enrollment Number : 26CSE001"<<endl;
+   cout<<"Enter Student Name : Akshar Patel"<<endl;
+   cout<<"Enter Branch : CSE"<<endl;
+   cout<<"Enter Semester : 1"<<endl;
+   cout<<"Enter Mobile Number : 9999999999"<<endl;
+   cout<<"--------------------------------";
+
+}

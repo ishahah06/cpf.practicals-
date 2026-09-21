52ddbdba3972413c25c7a672d3cf7c63a5faf80e
ishahah06 <shah.ishagpi@gmail.com>
2026-09-22 00:02:44 +0530

    Add files via upload

---
+++ practical 2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"**************"<<endl;
+   cout<<"  STUDENT RECORD MANAGEMENT SYSTEM"<<endl;
+   cout<<"**************"<<endl;
+   cout<<" Software Version : 1.1"<<endl;
+   cout<<" Institute : CHARUSAT University"<<endl;
+   cout<<" Academic Year : 2026-27"<<endl;
+   cout<<"----------------------------------"<<endl;
+   cout<<"Student Registration"<<endl;
+   cout<<"----------------------------------"<<endl;
+   string enroll_no,name;
+   cout<<"enter enrollment no" <<endl;
+   cin>>enroll_no;
+   cout<<"Enrollment no is"<<enroll_no<<endl;
+   string std_name;
+   cout<<"Enter student name"<<endl;
+   cin>>std_name;
+   cout<<"Student name is"<<std_name<<endl;
+   string branch;
+   cout<<"Enter branch"<<endl;
+   cin>>branch;
+   cout<<"Branch is"<<branch<<endl;
+   int semester;
+   cout<<"Enter semester"<<endl;
+   cin>>semester;
+   cout<<"semester is"<<semester<<endl;
+   int mobile_number;
+   cout<<"Enter mobile number"<<endl;
+   cin>>mobile_number;
+   cout<<"mobile number is"<<mobile_number<<endl;
+   cout<<"mobile number is"<<mobile_number<<endl;
+}
